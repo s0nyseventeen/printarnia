bb5cd5491c0a2848f20b9539a1b9e6316282ee9d
503282404 <stanislav.kryshchuk@ge.com>
2023-06-01 12:59:28 +0100

    Implemented login()

--- basic/auth.py
+++ basic/auth.py
@@ -4,10 +4,11 @@ from flask import (
     redirect,
     url_for,
     flash,
-    render_template
+    render_template,
+    session
 )
 from .db import get_db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -37,3 +38,26 @@ def register():
                 error = f'User {username} is already registered'
         flash(error)
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        user = get_db().execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        flash(error)
+    return render_template('auth/login.html')
