69b7041ec22c4fe135fdbe6549326c737fd74db4
503282404 <stanislav.kryshchuk@ge.com>
2023-06-01 17:13:17 +0100

    Added email field

--- basic/auth.py
+++ basic/auth.py
@@ -20,6 +20,7 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        email = request.form['email']
         db = get_db()
         error = None
 
@@ -27,12 +28,14 @@ def register():
             error = 'Username is required'
         elif not password:
             error = 'Password is required'
+        elif not email:
+            error = 'Email is required'
 
         if error is None:
             try:
                 db.execute(
-                    'INSERT INTO user (username, password) VALUES (?, ?)',
-                    (username, generate_password_hash(password))
+                    'INSERT INTO user (username, password, email) VALUES (?, ?, ?)',
+                    (username, generate_password_hash(password), email)
                 )
                 db.commit()
                 return redirect(url_for('auth.login'))
