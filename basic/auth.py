1e806a564f0b66bc8cb1e1af48648225c1248e9a
503282404 <stanislav.kryshchuk@ge.com>
2023-06-01 12:33:15 +0100

    Implemented register()

--- basic/auth.py
+++ basic/auth.py
@@ -1,3 +1,39 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    request,
+    redirect,
+    url_for,
+    flash,
+    render_template
+)
+from .db import get_db
+from werkzeug.security import generate_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+
+        if error is None:
+            try:
+                db.execute(
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+                return redirect(url_for('auth.login'))
+            except db.IntegrityError:
+                error = f'User {username} is already registered'
+        flash(error)
+    return render_template('auth/register.html')
