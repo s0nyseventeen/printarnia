bc3b7ae11818271c156b5a726f65bf6b8966daff
503282404 <stanislav.kryshchuk@ge.com>
2023-06-01 14:10:15 +0100

    Implemented before_app_request()

--- basic/auth.py
+++ basic/auth.py
@@ -5,7 +5,8 @@ from flask import (
     url_for,
     flash,
     render_template,
-    session
+    session,
+    g
 )
 from .db import get_db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -61,3 +62,15 @@ def login():
             return redirect(url_for('index'))
         flash(error)
     return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
