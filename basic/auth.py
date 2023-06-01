f37e475d36b242dfcc6b5ee2c7f44e0ec971dee1
503282404 <stanislav.kryshchuk@ge.com>
2023-06-01 14:25:01 +0100

    Implemented login_required()

--- basic/auth.py
+++ basic/auth.py
@@ -10,6 +10,7 @@ from flask import (
 )
 from .db import get_db
 from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -80,3 +81,13 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+
+def login_required(view):
+
+    @wraps(view)
+    def wrapper(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapper
