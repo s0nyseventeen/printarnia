41deef87614964eb773367c3aec97a281037bad3
503282404 <stanislav.kryshchuk@ge.com>
2023-06-03 12:10:19 +0100

    Implemented __create()

--- basic/gallery.py
+++ basic/gallery.py
@@ -1,5 +1,14 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for
+)
 from .db import get_db
+from .auth import login_required
+import datetime
 
 bp = Blueprint('gallery', __name__)
 
@@ -8,3 +17,30 @@ bp = Blueprint('gallery', __name__)
 def index():
     works = get_db().execute('SELECT * FROM work ORDER BY created;').fetchall()
     return render_template('gallery/index.html', works=works)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def __create():
+    if request.method == 'POST':
+        title = request.form['title']
+        created = datetime.datetime.now()
+        description = request.form['description']
+        image = request.form['image']
+        error = None
+
+        if not title:
+            error = 'Title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO work (title, created, description, image) '
+                'VALUES (?, ?, ?, ?)',
+                (title, created, description, image)
+            )
+            db.commit()
+            return redirect(url_for('gallery.index'))
+    return render_template('gallery/create.html')
