209718b74c2b0ee3776f1f1660b37d4c567bb299
sony <stanislav_kryshchuk@yahoo.com>
2023-06-10 14:00:33 +0300

    Modified in order to add tests

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.cfg')
+
+    if test_config is not None:
+        app.config.from_mapping(test_config)
 
     from .db import init_app
     init_app(app)
