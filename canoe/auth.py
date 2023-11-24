c576149125151ccecf50c940a0c8738c908b39dc
503282404 <stanislav.kryshchuk@ge.com>
2023-11-25 01:44:21 +0200

    Removed register logic

--- canoe/auth.py
+++ canoe/auth.py
@@ -1,60 +1,18 @@
 from functools import wraps
 
-from flask import Blueprint
-from flask import flash
 from flask import g
 from flask import redirect
 from flask import render_template
 from flask import request
 from flask import session
 from flask import url_for
-from psycopg2.errors import UniqueViolation
 from psycopg2.sql import Identifier
 from psycopg2.sql import Literal
 from psycopg2.sql import SQL
 from werkzeug.exceptions import abort
 from werkzeug.security import check_password_hash
-from werkzeug.security import generate_password_hash
 
 from .db import get_db
-
-bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        db = get_db()
-        error = None
-
-        if not username:
-            error = 'Username is required'
-        elif not password:
-            error = 'Password is required'
-        elif not email:
-            error = 'Email is required'
-
-        if not error:
-            try:
-                db.run_query(
-                    SQL("""
-                        INSERT INTO {table} (username, password, email)
-                        VALUES ({username}, {password}, {email});"""
-                    ).format(
-                        table=Identifier('users'),
-                        username=Literal(username),
-                        password=Literal(generate_password_hash(password)),
-                        email=Literal(email)
-                    )
-                )
-                return redirect(url_for('auth.login'))
-            except UniqueViolation:
-                error = f'User {username} is already registered'
-        flash(error)
-    return render_template('auth/register.html')
 
 
 @bp.route('/login', methods=('GET', 'POST'))
