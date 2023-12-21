1e52706ca2635a651c7a21d08918bee784eb7ff5
503282404 <stanislav.kryshchuk@gehealthcare.com>
2023-12-21 15:35:05 +0200

    Implement WorkImageList class in order to render all images of Work

--- src/gallery/update.py
+++ src/gallery/update.py
@@ -5,6 +5,7 @@ from flask import url_for
 
 from src.extensions import db
 from src.gallery import bp
+from src.gallery.models import Image
 from src.gallery.models import Work
 from src.lib.abstractview import AbstractView
 
@@ -31,7 +32,24 @@ class UpdateWork(AbstractUpdate):
         return redirect(url_for('gallery.detail', id=id))
 
 
+class WorkImageList(AbstractUpdate):
+    def __init__(self, model, image, template):
+        super().__init__(model, template)
+        self.__image = image
+
+    def dispatch_request(self, id):
+        return render_template(
+            self.template,
+            work=self.model.query.get_or_404(id),
+            images=self.__image.query.filter_by(work_id=id).order_by(Image.id).all()
+        )
+
+
 bp.add_url_rule(
     '/<int:id>/update',
     view_func=UpdateWork.as_view('UpdateWork', Work, 'update.html')
+)
+bp.add_url_rule(
+    '/<int:id>/update/images',
+    view_func=WorkImageList.as_view('WorkImageList', Work, Image, 'images_list.html')
 )
