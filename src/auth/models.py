0d735c395f5e8feaf2a3d3a25f47b56d55575f27
Stanislav Kryshchuk <stanislav_kryshchuk@yahoo.com>
2024-04-21 14:06:28 +0300

    Add uniqueness and not null for the Users' model title and email field in order to prevent inserting duplicates

--- src/auth/models.py
+++ src/auth/models.py
@@ -3,6 +3,6 @@ from src import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(256))
-    email = db.Column(db.String(256))
+    username = db.Column(db.String(256), unique=True, nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
     password = db.Column(db.String(256))
