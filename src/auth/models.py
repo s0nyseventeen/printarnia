00ccfab313b4355b88769851b0042ce9578073b1
Stanislav Kryshchuk <stanislav_kryshchuk@yahoo.com>
2024-04-21 13:35:12 +0300

    Modify the Users' model declaring approach to the Flask SQLAlchemy style in order to simplify the code

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,13 +1,8 @@
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import Mapped
-
 from src import db
 
 
 class Users(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String(256))
-    email: Mapped[str] = mapped_column(String(256))
-    password: Mapped[str] = mapped_column(String(256))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256))
+    email = db.Column(db.String(256))
+    password = db.Column(db.String(256))
