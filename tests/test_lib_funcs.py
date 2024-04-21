c73607992c2367347f5ca9d820ad9e990ae617f6
Stanislav Kryshchuk <stanislav_kryshchuk@yahoo.com>
2024-04-21 14:48:25 +0300

    Change Path object into str representation in order to pass on both Linux and Windows

--- tests/test_lib_funcs.py
+++ tests/test_lib_funcs.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 from src.lib.funcs import get_upload_path
 
 
 def test_get_upload_path(app):
     with app.app_context():
-        assert str(get_upload_path()) == 'src/static/images'
+        assert str(get_upload_path()) == str(Path('src/static/images'))
