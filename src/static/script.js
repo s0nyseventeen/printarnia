3c9b7022a45b3558a2b1862e664853356e96dc39
503282404 <stanislav.kryshchuk@gehealthcare.com>
2024-04-21 20:45:18 +0300

    Remove unnecessary logic in order to fix the detail link, which was intended to redirect to the main page

--- src/static/script.js
+++ src/static/script.js
@@ -11,18 +11,6 @@ const allLinks = document.querySelectorAll('a:link');
 
 allLinks.forEach((link) => {
 	link.addEventListener('click', (e) => {
-		const href = link.getAttribute('href');
-		if(href === '/'){
-			e.preventDefault();
-			window.scrollTo({top: 0, behavior: 'smooth'});
-		}
-
-		if(href !== '/' && href.startsWith('/#')){
-			e.preventDefault();
-			const sectionEl = document.querySelector(href.slice(1));
-			sectionEl.scrollIntoView({behavior: 'smooth'});
-		}
-
 		if(link.classList.contains('nav-link')){
 			headerInner.classList.toggle('nav-open');
 		}
